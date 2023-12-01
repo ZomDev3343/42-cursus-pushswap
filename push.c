655ac6ae11172dde43557c6f3feda566af6755b4
zom <contact.zomzd@gmail.com>
2023-12-01 12:44:40 +0100

    just keeping copy

--- push.c
+++ push.c
@@ -6,7 +6,7 @@
 /*   By: truello <truello@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 17:13:22 by truello           #+#    #+#             */
-/*   Updated: 2023/11/30 16:48:33 by truello          ###   ########.fr       */
+/*   Updated: 2023/12/01 12:40:54 by truello          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,16 @@
 
 void	pa(t_stack **a, t_stack **b)
 {
+	if (*b == NULL)
+		return ;
 	ft_printf("pa\n");
 	push(a, pop(b));
 }
 
 void	pb(t_stack **a, t_stack **b)
 {
+	if (*a == NULL)
+		return ;
 	ft_printf("pb\n");
 	push(b, pop(a));
 }
